e16720f4b42b7810eb232ba3cb8b6fc3558fad52
DoTruong112 <dotruong0108t@gmail.com>
2022-09-14 22:04:53 +0700

    Game Snake(Java Swing)
    Basic snake game code with java swing

---
+++ GameSnake/src/Model/Snake.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+/**
+ *
+ * @author 84375
+ */
+public class Snake extends JFrame {
+     public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+
+}
